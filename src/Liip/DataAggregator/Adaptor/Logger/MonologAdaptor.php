188e6bd1a2288745fad00d29cac9338bbaab0a6f
Bastian Feder <github@bastian-feder.de>
2013-10-04 10:44:04 +0200

    Realized that Monolog unfortunately is not compatible with psr/log/LoggerInterface, so added an adaptor for those who want to use monolog

---
+++ src/Liip/DataAggregator/Adaptor/Logger/MonologAdaptor.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Liip\DataAggregator\Adaptor\Logger;
+
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class LoggerMonologAdaptor
+ *
+ * @package NetMigrosIntranetDataAggregatorBundleAdaptor
+ */
+class MonologAdaptor implements LoggerInterface
+{
+    /**
+     * @var \Monolog\Logger
+     */
+    protected $monolog;
+
+    /**
+     * @param \Monolog\Logger $logger
+     */
+    public function __construct(Logger $logger)
+    {
+        $this->monolog = $logger;
+    }
+
+    /**
+     * System is unusable.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function emergency($message, array $context = array())
+    {
+        $this->monolog->emerg($message, $context);
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function alert($message, array $context = array())
+    {
+        $this->monolog->alert($message, $context);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function critical($message, array $context = array())
+    {
+        $this->monolog->srit($message, $context);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function error($message, array $context = array())
+    {
+        $this->monolog->err($message, $context);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function warning($message, array $context = array())
+    {
+        $this->monolog->warn($message, $context);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function notice($message, array $context = array())
+    {
+        $this->monolog->notice($message, $context);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return null
+     */
+    public function info($message, array $context = array())
+    {
+        $this->monolog->info($message, $context);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function debug($message, array $context = array())
+    {
+        $this->monolog->debug($message, $context);
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * Since there is no representation for the method in Monolog,
+     * Monolog::info() is used.
+     *
+     * @param mixed  $level
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function log($level, $message, array $context = array())
+    {
+        $this->info($level, $message, $context);
+    }
+}
